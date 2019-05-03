08a6d673df86b7e60098f32328eaf0b9bda28009
AdityaGoyal1999 <44474678+AdityaGoyal1999@users.noreply.github.com>
2019-05-04 01:20:03 +0530

    Add files via upload

---
+++ BodyPart.java
@@ -0,0 +1,37 @@
+public class BodyPart
+{
+    
+    private int xCoor, yCoor, width, height;
+    
+    public BodyPart()
+    {
+        this.xCoor = xCoor;
+        this.yCoor = yCoor;
+        width = tileSize;
+        height = tileSize;
+    }
+    public void tick()
+    {
+    }
+    public void draw(Graphics g)
+    {
+        g.setColor(Color.YELLOW);
+        g.fillRect(xCoor*width, yCoor*height, width, height);
+    }
+    public void getxCoor()
+    {
+        return xCoor;
+    }
+    public void setxCoor(int xCoor)
+    {
+        this.xCoor = xCoor;
+    }
+    public int getyCoor()
+    {
+        return yCoor;
+    }
+    public void setyCoor(int yCoor)
+    {
+        this.yCoor = yCoor;
+    }
+}
